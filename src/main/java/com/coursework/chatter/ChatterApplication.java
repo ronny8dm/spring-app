c715f7949dc4ee655aac3ba3b94151848f762639
SolusRGB <brandoneddycode@gmail.com>
2023-10-09 16:53:19 +0100

    Initial commit

--- src/main/java/com/coursework/chatter/ChatterApplication.java
+++ src/main/java/com/coursework/chatter/ChatterApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChatterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChatterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChatterApplication.class, args);
+        System.out.println("Hello World!");
+    }
 
 }
